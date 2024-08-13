a8033a00aef8caa76cfdb53f90dce5a7027daaab
natishget <natishget@gmail.com>
2024-08-13 16:34:14 +0300

    my commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,14 @@ def number_of_subscribers(subreddit):
        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-      "User_Agent": "windows11:0X16.api.advanced/1.0 (by /u/natishget_33)"
+      "User-Agent": "windows11:0X16.api.advanced/1.0 (by /u/natishget_33)"
     }
     try:
        req = requests.get(url, headers=headers, allow_redirects=False)
-       if req.response.status_code == 400:
+       if req.status_code == 400:
           return 0
        result = req.json().get("data")
-       return result.get("subscriber")
+       return result.get("subscribers")
     except (Exception):
        print('Not Found')
        return (0)
