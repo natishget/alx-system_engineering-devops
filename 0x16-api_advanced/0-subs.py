17def2a7aeae88669e7e1b53bb2d4335712a3e6a
natishget <126060067+natishget@users.noreply.github.com>
2024-08-13 16:36:16 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""query number of subscriber in a reddit"""
+"""Query the number of subscribers in a Reddit subreddit"""
 
-"""importing request to make http request to a server"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """number_of_subscribers() function returns number of subscribers on reddit"""
+    """Returns the number of subscribers for a given Reddit subreddit"""
     if subreddit is None or type(subreddit) is not str:
-       return 0
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-      "User-Agent": "windows11:0X16.api.advanced/1.0 (by /u/natishget_33)"
+        "User-Agent": "windows11:0X16.api.advanced/1.0 (by /u/natishget_33)"
     }
+
     try:
-       req = requests.get(url, headers=headers, allow_redirects=False)
-       if req.status_code == 400:
-          return 0
-       result = req.json().get("data")
-       return result.get("subscribers")
-    except (Exception):
-       print('Not Found')
-       return (0)
-      
+        req = requests.get(url, headers=headers, allow_redirects=False)
+        if req.status_code == 400:
+            return 0
+        result = req.json().get("data")
+        return result.get("subscribers", 0)
+    except requests.exceptions.RequestException:
+        print('Request failed')
+        return 0
+        
