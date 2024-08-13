2de7a7b47956b93c9ce0bb996042ed4ec84c8f08
natishget <natishget@gmail.com>
2024-08-13 16:17:57 +0300

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""query number of subscriber in a reddit"""
+
+"""importing request to make http request to a server"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """number_of_subscribers() function returns number of subscribers on reddit"""
+    if subreddit is None or type(subreddit) is not str:
+       return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+      "User_Agent": "windows11:0X16.api.advanced/1.0 (by /u/natishget_33)"
+    }
+    try:
+       req = requests.get(url, headers=headers, allow_redirects=False)
+       if req.response.status_code == 400:
+          return 0
+       result = req.json().get("data")
+       return result.get("subscriber")
+    except (Exception):
+       print('Not Found')
+       return (0)
+      
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # alx-system_engineering-devops
-# 0x16. API advanced
