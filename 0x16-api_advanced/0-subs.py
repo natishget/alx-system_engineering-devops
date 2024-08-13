56ce62e34cb82c604e779de707018b0f3bd1419a
natishget <natishget@gmail.com>
2024-08-13 17:11:20 +0300

    message4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Query the number of subscribers in a Reddit subreddit""".
+
+"""Query the number of subscribers in a Reddit subreddit."""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given Reddit subreddit""".
-    if subreddit is None or type(subreddit) is not str:
+    """Return the number of subscribers for a given Reddit subreddit."""
+    if not isinstance(subreddit, str) or subreddit is None:
         return 0
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "windows11:0X16.api.advanced/1.0 (by /u/natishget_33)"
     }
 
     try:
-        req = requests.get(url, headers=headers, allow_redirects=False)
-        if req.status_code == 400:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 400:
             return 0
-        result = req.json().get("data")
+        result = response.json().get("data", {})
         return result.get("subscribers", 0)
     except requests.exceptions.RequestException:
         print('Request failed')
